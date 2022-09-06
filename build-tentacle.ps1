63099775d0468889b4cef803019cada8b698d100
max <max@tfq.at>
2022-09-07 00:15:33 +0200

    add packed version

--- build-tentacle.ps1
+++ build-tentacle.ps1
@@ -1,6 +1,7 @@
 cd "source/frontend"
 npm run build
 cd ..
-cd ..
 robocopy "source\backend\tentacles" "build\OctoBot-Tentacle\tentacles\" /MIR
 robocopy "source\frontend\build" "build\OctoBot-Tentacle\tentacles\Services\Interfaces\octo_ui2\static\" /MIR
+
+echo "use this command to build a tentacle package: 'python path/to/my/OctoBot/start.py tentacles -p build\any_platform.zip -d build/OctoBot-Tentacle/tentacles'"
