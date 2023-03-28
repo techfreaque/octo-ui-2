0b475be06b1e70cc0dd2cb41de85fbd4fefb51e3
eugeborzone <eugenioborzone@gmail.com>
2023-03-28 13:20:48 -0300

    change RefreshData button

--- source/frontend/src/widgets/AppWidgets/Buttons/RefreshData.js
+++ source/frontend/src/widgets/AppWidgets/Buttons/RefreshData.js
@@ -1,21 +1,38 @@
 import { Button } from "@mui/material";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRefresh } from "@fortawesome/free-solid-svg-icons";
-import { useMemo, useState } from "react";
+import { useMemo, useState, useEffect } from "react";
 import { useFetchBotInfo } from "../../../context/data/BotInfoProvider";
 import { useIsBotOnlineContext } from "../../../context/data/IsBotOnlineProvider";
+import { SyncOutlined } from "@ant-design/icons";
+
 
 export default function RefreshBotData() {
   const fetchBotInfo = useFetchBotInfo()
   const botIsOnline = useIsBotOnlineContext()
   const [isFinished, setIsFinished] = useState(true)
-  const isFetching = !isFinished || !botIsOnline
+  const [isSearching, setIsSearching] = useState(false);
+
+  const isFetching = !isFinished || !botIsOnline || isSearching;
+
+  useEffect(() => {
+    let timer;
+    if (isSearching) {
+      timer = setTimeout(() => {
+        setIsSearching(false);
+      }, 1800);
+    }
+    return () => clearTimeout(timer);
+  }, [isSearching]);
+
   return useMemo(() => {
     return (
-      <Button disabled={isFetching} onClick={() => fetchBotInfo(true, setIsFinished)} >
-        <FontAwesomeIcon icon={faRefresh} size="2xl" className={isFetching ? "fa-spin" : ""} />
+      <Button disabled={isFetching} onClick={() => {
+        setIsSearching(true);
+        fetchBotInfo(true, setIsFinished);
+      }}>
+        <SyncOutlined spin={isSearching} style={{fontSize:'24px'}}/>
       </Button>
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isFinished, botIsOnline])
+  }, [isFinished, botIsOnline, isSearching])
 }
+
