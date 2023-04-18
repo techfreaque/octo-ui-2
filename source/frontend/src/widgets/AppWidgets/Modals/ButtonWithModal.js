15696a685ec74e7232f81b57f8f1e6eb327c165a
eugeborzone <eugenioborzone@gmail.com>
2023-04-18 09:12:33 -0300

    ButtonWithModal

--- source/frontend/src/widgets/AppWidgets/Modals/ButtonWithModal.js
+++ source/frontend/src/widgets/AppWidgets/Modals/ButtonWithModal.js
@@ -2,26 +2,7 @@ import {useState} from "react";
 import AppWidgets from '../../WidgetManagement/RenderAppWidgets';
 import IconFromString from '../../../components/Icons/IconFromString';
 import {Tooltip, Modal} from 'antd';
-import {useBotColorsContext} from '../../../context/config/BotColorsProvider';
 import AntButton from '../../../components/Buttons/AntButton';
-
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    maxWidth: 1000,
-    width: "100%",
-    // bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    color: "white",
-    p: 4,
-    overflowY: "auto",
-    maxHeight: "100vh"
-};
-
 
 export default function ButtonWithModal({
     title,
@@ -62,42 +43,26 @@ export default function ButtonWithModal({
 }
 
 function ModalContent({open, id, handleClose, content}) {
-    const botColors = useBotColorsContext()
     return (
-    <Modal 
-        open={open}
-        onCancel={handleClose}
-        width={1000}
-        centered
-        footer={null}
-        closable={true}
-        // closable={false}
-        keyboard
-        style={
-            {zIndex: "1000"}
-        }
-        >
-        <div  style={
-            {
-                ...style,
-                backgroundColor: botColors?.background,
-                maxHeight: "600px", 
-                padding: '20px 32px 0 32px'
-            }
-        }>
-            {/* <AntButton onClick={handleClose}
-                buttonVariant="text"
-                style={
-                    {
-                        float: "right",
-                        zIndex: 100
-                    }
+        <Modal 
+            open={open}
+            onCancel={handleClose}
+            width={1000}
+            centered
+            footer={null}
+            closable={true}
+            // closable={false}
+            keyboard
+            zIndex={1000}
+            >
+            <div  style={
+                {
+                    padding: '20px 32px 0 32px'
                 }
-                antIcon={"CloseOutlined"}
-            ></AntButton> */}
-            {
-            content && <AppWidgets layout={content}/>
-        }
-        </div>
-    </Modal>)
+            }>
+                {
+                content && <AppWidgets layout={content}/>
+            }
+            </div>
+        </Modal>)
 }
